bc465df590a30ecf2ecd0743f455d2d704230d87
ainyko <977669079@qq.com>
2016-08-16 17:04:59 +0800

    1.test tortoiseGit;

--- ZedControl/MyTabControl.cs
+++ ZedControl/MyTabControl.cs
@@ -16,7 +16,7 @@ namespace TrafficInfoManagerUI
         public MyTabControl()
         {
 
-            base.SetStyle(ControlStyles.UserPaint |                      // 控件将自行绘制，而不是通过操作系统来绘制  
+            base.SetStyle(ControlStyles.UserPaint |                  // 控件将自行绘制，而不是通过操作系统来绘制  
                      ControlStyles.OptimizedDoubleBuffer |          // 该控件首先在缓冲区中绘制，而不是直接绘制到屏幕上，这样可以减少闪烁  
                      ControlStyles.AllPaintingInWmPaint |           // 控件将忽略 WM_ERASEBKGND 窗口消息以减少闪烁  
                      ControlStyles.ResizeRedraw |                   // 在调整控件大小时重绘控件  
